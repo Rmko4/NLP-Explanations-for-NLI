6e36979acc96c99eb05cdde5bd594e2a47b2651d
Remco Leijenaar <remco-leijenaar@live.com>
2023-04-05 20:41:40 +0200

    Prediction results now have different column order and integer labels converted to string labels

--- sandbox/merge_csv.py
+++ sandbox/merge_csv.py
@@ -5,22 +5,40 @@ from pathlib import Path
 path = Path('../results')
 
 # %%
-# Get file with using wildcard
+# Get files using wildcard
 path_full = list(path.glob('Testing_*Full*.csv'))
 path_lora = list(path.glob('Testing_*Lora*.csv'))
 # %%
 df1 = pd.read_csv(path_full[0])
 df2 = pd.read_csv(path_full[1])
 # %%
+# Swap the two columns in df1
+df1 = df1[['predicted_label', 'reference_label']]
+
+# %%
+import datasets
+# %%
+int2str_label = datasets.load_dataset("esnli")['test'].features['label'].int2str
+# %%
+# Map the int2str_label function to 'reference_label and 'predicted_label' columns
+df1['reference_label'] = df1['reference_label'].map(int2str_label)
+df1['predicted_label'] = df1['predicted_label'].map(int2str_label)
+# %%
 merged_df = pd.concat([df2, df1], axis=1)
 
 # %%
 merged_df.head()
 # %%
-merged_df.to_csv(path/'Predictions_Full', index=False)
+merged_df.to_csv(path/'Predictions_Full.csv', index=False)
 # %%
 df1 = pd.read_csv(path_lora[0])
 df2 = pd.read_csv(path_lora[1])
+df1 = df1[['predicted_label', 'reference_label']]
+df1['reference_label'] = df1['reference_label'].map(int2str_label)
+df1['predicted_label'] = df1['predicted_label'].map(int2str_label)
 merged_df = pd.concat([df2, df1], axis=1)
-merged_df.to_csv(path/'Predictions_Lora', index=False)
+merged_df.to_csv(path/'Predictions_Lora.csv', index=False)
 # %%
+merged_df.head()
+# %%
+pass
