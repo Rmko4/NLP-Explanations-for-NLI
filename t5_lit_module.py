dd524829074424903677709201c85665865932c9
Remco Leijenaar <remco-leijenaar@live.com>
2023-04-05 15:53:52 +0200

    Removed args on_test_epoch_end

--- t5_lit_module.py
+++ t5_lit_module.py
@@ -193,7 +193,7 @@ class LitT5(LightningModule):
 
         return output
 
-    def on_test_epoch_end(self, outputs):
+    def on_test_epoch_end(self):
         # Log test blue score
         blue_score = self.bleu_metric.compute()
         self.log('test/blue', blue_score)
